412e593f4135415174ef94cd10b734e533981d7f
TheMr3thicIsPro <broyal0110@icloud.com>
2026-01-30 19:03:56 +1100

    Change trial duration to 1 minute for lockout testing

--- supabase/migrations/20260115000002_add_trial_expiration_to_profiles.sql
+++ supabase/migrations/20260115000002_add_trial_expiration_to_profiles.sql
@@ -18,7 +18,7 @@ BEGIN
     -- If plan is being changed to 'free' and was not previously 'free', start trial
     ELSIF NEW.plan = 'free' AND COALESCE(OLD.plan, '') != 'free' THEN
         NEW.trial_started_at := COALESCE(NEW.trial_started_at, NOW());
-        NEW.trial_expires_at := NEW.trial_started_at + INTERVAL '5 minutes';
+        NEW.trial_expires_at := NEW.trial_started_at + INTERVAL '1 minute';
         NEW.account_locked := FALSE;
     END IF;
 
@@ -42,8 +42,8 @@ SET
     trial_expires_at = COALESCE(trial_expires_at, 
         CASE 
             WHEN created_at IS NOT NULL 
-            THEN created_at + INTERVAL '5 minutes'
-            ELSE NOW() + INTERVAL '5 minutes'
+            THEN created_at + INTERVAL '1 minute'
+            ELSE NOW() + INTERVAL '1 minute'
         END)
 WHERE plan = 'free' AND trial_expires_at IS NULL;
 
